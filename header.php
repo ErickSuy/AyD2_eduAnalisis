b04433424c69fe19186b0e86b1834a069f6b8be3
201213295 <201213295a@ingenieria.usac.edu.gt>
2016-03-07 19:25:15 -0600

    Modificaciones al Css

--- header.php
+++ header.php
@@ -19,28 +19,36 @@ if (!isset($_SESSION['nombre_Usuario'])){
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!--<script src="./javascripts/verificaciones.js"></script>
 <script src="./javascripts/login.js"></script>-->
-<title>BANCAUSAC</title>
+<title>EduAnalisis</title>
 </head>
 <body>
     <div class="banner">
-       <a href="../principal.php"><img src="../images/banner.png"></a>
-    </div>
-        <div class="container">
+        <div class="image_banner">
+       <a href="./principal.php"><img src="./images/banner.png"></a>
+       </div>
+       <div class="container_sub">
             <ul id="nav">
-                <li><a href="../principal.php" > Principal </a></li>
-                <li id="li_afiliados"><a href="#s1">Afiliados</a>
+                <li><a href="./principal.php" > Principal </a></li>
+                <li><a href="./principal.php" > Acerca De </a></li>
+                <li><a href="php/logout.php" class="fa fa-toggle-off"><?php echo " ".$_SESSION['nombre_Usuario']; ?> Logout</a></li>
+            </ul>
+       </div>
+        <div class="div_clear"></div>
+        <div class="container">        
+            <ul id="nav">
+
+                <li id="li_afiliados"><a href="#s1">Maestros</a>
                     <span id="s1"></span>
                     <ul class="subs">
-                        <li><a href="#">Gestion de Afiliados</a>
+                        <li><a href="#">Gestion de Maestros</a>
                             <ul>
-                                <li id="li_tipo_afiliado_fe"><a href="../abc/tipo_afiliado_fe.php">Operaciones Sobre Tipos de Afiliados</a></li>
-                                <li id="li_afiliado_fe"><a href="../abc/afiliado_fe.php">Operaciones Sobre Afiliados</a></li>
-
+                                <li id="li_maestros_fe"><a href="./abc/maestros_fe.php">Operaciones Sobre Maestros</a></li>
                             </ul>
                         </li>
                     </ul>
                 </li>
-                <li id="li_clientes"><a href="#s2">Clientes</a>
+
+                <li id="li_clientes"><a href="#s2">Alumnos</a>
                     <span id="s2"></span>
                     <ul class="subs">
                         <li><a href="#">Gestion de Clientes</a>
@@ -51,7 +59,8 @@ if (!isset($_SESSION['nombre_Usuario'])){
                         </li>
                     </ul>
                 </li>
-                <li id="li_Cuenta"><a href="#s3">Cuentas</a>
+
+                <li id="li_Cuenta"><a href="#s3">Cursos</a>
                     <span id="s3"></span>
                     <ul class="subs">
                         <li><a href="#">Gestion de Cuentas</a>
@@ -63,7 +72,8 @@ if (!isset($_SESSION['nombre_Usuario'])){
                         </li>
                     </ul>
                 </li>
-                <li id="li_creditos"><a href="#s4">Creditos</a>
+
+                <li id="li_creditos"><a href="#s4">Asignaciones</a>
                     <span id="s4"></span>
                     <ul class="subs">
                         <li><a href="#">Gestion de Creditos</a>
@@ -76,10 +86,10 @@ if (!isset($_SESSION['nombre_Usuario'])){
                         </li>
                     </ul>
                 </li>
-                <li id="li_autorizaciones"><a href="#s5">Autorizaciones</a>
+                <li id="li_autorizaciones"><a href="#s5">Horarios</a>
                     <span id="s5"></span>
                     <ul class="subs">
-                        <li><a href="#">Gestion de Autorizaciones</a>
+                        <li><a href="#">Horarios</a>
                             <ul>
                                 <li id="li_autorizacion_fe"><a href="../Opciones/autorizacion_fe.php">Autorizacion</a></li>
                                 <li id="li_retiro_fe"><a href="../Opciones/retiro_fe.php">Retiro efectivo</a></li>
@@ -89,83 +99,8 @@ if (!isset($_SESSION['nombre_Usuario'])){
                         </li>
                     </ul>
                 </li>
-                <li id="li_caja"><a href="#s6">Caja</a>
-                    <span id="s6"></span>
-                    <ul class="subs">
-                        <li><a href="#">Gestion de Caja</a>
-                            <ul>
-                                <li id="li_pagos_fe"><a href="../caja/pagos_fe.php"> Pagos de Cuentas </a></li>
-                                <li id="li_transferencia_fe"><a href="../caja/transferencia_fe.php"> Transferencias </a></li>
-                                <li id="li_transferencia_fe"><a href="../caja/notas_credito_fe.php"> Notas de Credito </a></li>
-                                <li id="li_transferencia_fe"><a href="../caja/notas_debito_fe.php"> Notas de Debito </a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-                <li id="li_autidor"><a href="#s7">Auditor</a>
-                    <span id="s7"></span>
-                    <ul class="subs">
-                        <li><a href="#">Senda de Auditoria</a>
-                            <ul>
-                                <li id="li_consultas_fe"><a href="../auditor/senda_auditoria_fe.php"> Consultas </a></li>
-                                <li id="li_reportes_fe"><a href="../auditor/transferencia_fe.php"> Reportes </a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-                <li id="li_grabador"><a href="#s8"> Grabador </a>
-                    <span id="s8"></span>
-                    <ul class="subs">
-                        <li><a href="#"> Gestion de Grabador </a>
-                            <ul>
-                                <li id="li_grab_voucher_fe"><a href="../operador_grabador/lote_voucher_fe.php"> Lotes de Voucher </a></li>
-                                <li id="li_grab_retiro_fe"><a href="../operador_grabador/lote_retiros_fe.php"> Lotes de Retiros </a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-                <li id="li_operador"><a href="#s9"> Operador </a>
-                    <span id="s9"></span>
-                    <ul class="subs">
-                        <li><a href="#"> Gestion de Operador</a>
-                            <ul>
-                                <li id="li_grab_voucher_fe"><a href="../operador_grabador/op_lote_voucher_fe.php"> Lotes de Voucher </a></li>
-                                <li id="li_grab_retiro_fe"><a href="op_lote_retiro_fe.php"> Lotes de Retiros </a></li>
-                                <li id="li_corte_fe"><a href="../operador_grabador/corte_fe.php"> Corte de la tarjeta </a></li>
-                                <li id="li_emision_fe"><a href="../operador_grabador/emision_estado_fe.php"> Emision de estados de cuenta </a></li>
-                                <li id="li_cierre_fe"><a href="../operador_grabador/cierre_fe.php"> Cierre de la tarjeta </a></li>
-                                <li id="li_lista_fe"><a href="../operador_grabador/lista_negra_fe.php"> Emision de lista negra </a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-                <li id="li_gerente"><a href="#s10"> Gerente </a>
-                    <span id="s10"></span>
-                    <ul class="subs">
-                        <li><a href="#"> Gestion de Gerente </a>
-                            <ul>
-                                <li id="li_consulta_saldo"><a href="../gerente/grafica_emisor_fe.php"> Consulta saldos por emisor </a></li>
-                                <li id="li_consulta_evolucion"><a href="../gerente/grafica_evolucion_fe.php"> Consulta evolucion de saldos promedio </a></li>
-                                <li id="li_consulta_emisor"><a href="../gerente/consulta_emisor.php"> Consulta emisores </a></li>
 
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-                <li id="li_dba"><a href="#s11"> DBA </a>
-                    <span id="s11"></span>
-                    <ul class="subs">
-                        <li><a href="#"> Operaciones DBA </a>
-                            <ul>
-                                <li id="li_tasa_interes"><a href="../dba/interes_fe.php"> Tasas de Interes </a></li>
-                                <li id="li_emisores"><a href="../creditos/emisores_fe.php"> Emisores </a></li>
-                                <li id="li_usuarios"><a href="../dba/usuarios_fe.php"> Usuarios </a></li>
-                                <li id="li_backup"><a href="../php/backup_base.php"> back up Base de Datos </a></li>
-
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-                <li><a href="php/logout.php" class="fa fa-toggle-off"><?php echo " ".$_SESSION['nombre_Usuario']; ?> Logout</a></li>
+                 
             </ul>
         </div>
+</div>
--- principal.php
+++ principal.php
@@ -14,30 +14,14 @@ if(!isset($_SESSION)){
 
 
 <div id="div_cuerpo">
-    	<h1> BIENVENIDO BANCAUSAC </h1>
+    	<h1> BIENVENIDO EDUANALISIS </h1>
 
-    	<div class="div_clear"></div>
-    	<div class="div_input">
-			TIPO DE CAMBIO <br>
-			Agencia<br>
-			Compra: Q7.54<br>
-			Venta : Q7.74<br>
-		</div>
-
-		<div aling="justify">
-		    <p style="font-size: 150%;"> Ayudamos a que tu dinero crezca por medio de nuestras
-		    cuentas de ahorro, de cheques e inversiones.
-		     Si lo que necesitas es credito, ponemos a tu disposicion
-		     nuestros prestamos y tarjetas, con los mejores planes
-		     de financiamiento de acuerdo a tus necesidades. </p>
-
-
-		</div>
+    	
 
  <div class="div_clear"></div>
 
 <div >
-        		<a><img src="https://portal.ingenieria.usac.edu.gt/templates/fiusactemplate/images/banner_principal.jpg"></a>
+        		<a></a>
     		</div>
 </div>
 </div>
